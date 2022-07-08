cbd6e47fe040bdd88149405c8f0150456d6c4552
Oleg Gnidets <oleg.gnidets@okta.com>
2022-07-08 17:39:00 +0300

    Concurrency

--- ReaddleSample1/ViewController.swift
+++ ReaddleSample1/ViewController.swift
@@ -8,9 +8,9 @@
 import UIKit
 
 /* Horizontal View
-    H: 150
+ H: 150
  
-    Vertical center
+ Vertical center
  */
 
 class CollectionItem: UICollectionViewCell {
@@ -18,44 +18,69 @@ class CollectionItem: UICollectionViewCell {
     
 }
 
-class ViewController: UIViewController {
+class ItemsDataSource {
     
-    @IBOutlet private weak var collectionView: UICollectionView!
+    private var _numberOfItems: Int = 0
+    
+    var numberOfItems: Int {
+        queue.sync {
+            _numberOfItems
+        }
+    }
+    
+    private let queue = DispatchQueue(label: "ItemsDataSource.queue")
+    
+    func removeItem() {
+        queue.sync {
+            _numberOfItems -= 1
+        }
+    }
+    
+    func addItem() {
+        queue.sync {
+            _numberOfItems += 1
+        }
+    }
+}
+
+class ViewController: UIViewController {
     
     private var numberOfItems = 0
     
+    var source = ItemsDataSource()
+    
+    @IBOutlet private weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         collectionView.delegate = self
         collectionView.dataSource = self
     }
     
-    @IBAction private func addItem() {
-        numberOfItems += 1
+    func removeItem(at index: Int) {
+        source.removeItem()
+        
+        collectionView.deleteItems(at: [IndexPath(item: index, section: 0)])
+    }
+    
+    @IBAction func addItem() {
+        source.addItem()
         
         collectionView.reloadData()
-        
         collectionView.scrollToItem(at: IndexPath(item: numberOfItems - 1, section: 0), at: .right, animated: true)
-    }
-}
-
-extension ViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        CGSize(width: 150, height: 150)
     }
 }
 
 extension ViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        numberOfItems -= 1
-        collectionView.deleteItems(at: [indexPath])
+        removeItem(at: indexPath.item)
     }
 }
 
 extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        numberOfItems
+        source.numberOfItems
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -68,6 +93,6 @@ extension ViewController: UICollectionViewDataSource {
         return cell
     }
     
-
+    
 }
 
