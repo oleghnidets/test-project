54e825c4ea3a8ebe0d1b65164eb0f9cdf203404c
Oleg Gnidets <oleg.gnidets@okta.com>
2022-07-08 18:00:57 +0300

    Pan Touch

--- ReaddleSample1/ViewController.swift
+++ ReaddleSample1/ViewController.swift
@@ -51,6 +51,8 @@ class ViewController: UIViewController {
     
     @IBOutlet private weak var collectionView: UICollectionView!
     
+    private var initialCenter: CGPoint = .zero
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -70,6 +72,39 @@ class ViewController: UIViewController {
         collectionView.reloadData()
         collectionView.scrollToItem(at: IndexPath(item: numberOfItems - 1, section: 0), at: .right, animated: true)
     }
+    
+    @objc func moveCell(_ gestureRecognizer: UIPanGestureRecognizer) {
+        guard let cell = gestureRecognizer.view else {
+            return
+        }
+        
+        let translation = gestureRecognizer.translation(in: view)
+
+        
+        if gestureRecognizer.state == .began {
+            self.initialCenter = cell.center
+        }
+        
+        if gestureRecognizer.state == .ended {
+            if translation.y < 50 && translation.y > -50 {
+                cell.center = initialCenter
+                return
+            } else {
+                guard let indexPath = collectionView.indexPathForItem(at: initialCenter) else {
+                    return
+                }
+                
+                removeItem(at: indexPath.item)
+            }
+        }
+
+        if gestureRecognizer.state != .cancelled {
+            let newCenter = CGPoint(x: initialCenter.x, y: initialCenter.y + translation.y)
+            cell.center = newCenter
+        } else {
+            cell.center = initialCenter
+        }
+    }
 }
 
 extension ViewController: UICollectionViewDelegate {
@@ -88,11 +123,11 @@ extension ViewController: UICollectionViewDataSource {
             return UICollectionViewCell()
         }
         
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(moveCell(_:)))
+        cell.addGestureRecognizer(panGesture)
+        
         cell.contentView.backgroundColor = .green
         
         return cell
     }
-    
-    
 }
-
